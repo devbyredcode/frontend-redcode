d2a12ef1104f3962fd9aada10da85cc516fbc1e1
egajavier <egaharwenda@gmail.com>
2020-08-14 07:30:05 +0700

    [FE] Add Skeleton on Detail Story

--- src/pages/story-page-detail/index.js
+++ src/pages/story-page-detail/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import StoryPageDetailStyle from './index.style';
 import HeaderPrimary from '../../components/header-primary';
-import LoadingPrimary from '../../components/loading-primary';
+import Skeleton from 'react-loading-skeleton';
 import BreadcrumbsPrimary from '../../components/breadcrumbs-primary';
 import FooterSection from '../../section/footer-section';
 import axios from '../../axios';
@@ -46,40 +46,59 @@ class StoryPageDetail extends Component{
     render(){
         return(
             <React.Fragment>
-                {this.state.isLoading ?
-                <LoadingPrimary/> :
                 <StoryPageDetailStyle>
                     <HeaderPrimary
                         url="/story"
                         title="STORY"/>
                     <div className="story-page">
                         <div className="container story-page__title">
-                            <BreadcrumbsPrimary
-                                root="/story"
-                                root_name="story"
-                                title={this.state.itemData.title}
-                            />
-                            <h1>{this.state.itemData.title}</h1>
-                            <p><span className="category">{this.state.itemData.category}</span> by {this.state.itemData.author}</p>
+                            {
+                                this.state.isLoading ? 
+                                    <Skeleton/> :
+                                    <BreadcrumbsPrimary
+                                        root="/story"
+                                        root_name="story"
+                                        title={this.state.itemData.title}/>
+                            }
+                            {
+                                this.state.isLoading ? 
+                                    <Skeleton height={25} count={2} style={{margin: "20px 0px 0px"}}/> :
+                                    <h1>{this.state.itemData.title}</h1>
+                            }
+                            {
+                                this.state.isLoading ? 
+                                    "" :
+                                    <p><span className="category">{this.state.itemData.category}</span> by {this.state.itemData.author}</p>
+                            }
                         </div>
                         <div className="container story-page__image">
-                            <img src={`https://res.cloudinary.com/devbyredcode/image/upload/${this.state.itemData.image}`} className="img-fluid" alt={this.state.itemData.title}/>
+                            {
+                                this.state.isLoading ? 
+                                    <Skeleton height={300}/> :
+                                    <img src={`https://res.cloudinary.com/devbyredcode/image/upload/${this.state.itemData.image}`} className="img-fluid" alt={this.state.itemData.title}/>
+                            }
                         </div>
                         <div className="container story-page__description">
-                            <p className="" dangerouslySetInnerHTML={{__html: this.state.itemData.detail}}/>
+                            {
+                                this.state.isLoading ? 
+                                    <Skeleton height={15} count={10} style={{marginBottom: "15px"}}/> :
+                                    <p className="" dangerouslySetInnerHTML={{__html: this.state.itemData.detail}}/>
+                            }
                         </div>
-                        <div className="container story-page__share">
-                            <p>Kalau temen-temen merasa tulisan ini bermanfaat? silahkan bagikan melalui social media agar dapat bermanfaat untuk temen-temen lainya:</p>
-                            <EmailShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut ${window.location.href} dan semoga bermanfaat`}><EmailIcon size={32}/></EmailShareButton>
-                            <TelegramShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut : ${window.location.href} dan semoga bermanfaat`}><TelegramIcon size={32}/></TelegramShareButton>
-                            <WhatsappShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut : ${window.location.href} dan semoga bermanfaat`}><WhatsappIcon size={32}/></WhatsappShareButton>
-                            <TwitterShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut : ${window.location.href} dan semoga bermanfaat`}><TwitterIcon size={32}/></TwitterShareButton>
-                            <WorkplaceShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut : ${window.location.href} dan semoga bermanfaat`}><WorkplaceIcon size={32}/></WorkplaceShareButton>
-                        </div>
+                        {
+                            this.state.isLoading ? "" : 
+                            <div className="container story-page__share">
+                                <p>Kalau temen-temen merasa tulisan ini bermanfaat? silahkan bagikan melalui social media agar dapat bermanfaat untuk temen-temen lainya:</p> 
+                                <EmailShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut ${window.location.href} dan semoga bermanfaat`}><EmailIcon size={32}/></EmailShareButton>
+                                <TelegramShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut : ${window.location.href} dan semoga bermanfaat`}><TelegramIcon size={32}/></TelegramShareButton>
+                                <WhatsappShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut : ${window.location.href} dan semoga bermanfaat`}><WhatsappIcon size={32}/></WhatsappShareButton>
+                                <TwitterShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut : ${window.location.href} dan semoga bermanfaat`}><TwitterIcon size={32}/></TwitterShareButton>
+                                <WorkplaceShareButton url={`Ayo baca artikel berikut, yang berjudul ${this.textToCapitalize(this.state.itemData.title)} dengan klik link berikut : ${window.location.href} dan semoga bermanfaat`}><WorkplaceIcon size={32}/></WorkplaceShareButton>
+                            </div>
+                        }
                     </div>
                 <FooterSection/>
                 </StoryPageDetailStyle>
-                }
             </React.Fragment>
         )
     }
